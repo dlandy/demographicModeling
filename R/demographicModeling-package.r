ee91d1f1e72abd1789466df6ddb80e111c4de271
David Landy <dhlandy@gmail.com>
2018-01-25 17:12:15 -0500

    added package documentation

---
+++ R/demographicModeling-package.r
@@ -0,0 +1,21 @@
+#' demographicModeling.
+#'
+#' This package provides a number of tools we have found useful in modeling demographic estimations, and other kinds of estimations.
+#' This includes one set of functions to calculate, given tibble column names, Della-Carpini & Keeter's political knowledge
+#' assessment, the BNT (coming soon), to recode on political ideology (coming soon), and to calculate and recode 
+#' other standard measures. 
+#' 
+#' It includes a set of functions to build and regularize graphs, and third set of functions to simplify running JAGS models 
+#' over the kinds of data we typically collect from qualtrics, ipsosMORI, and GSS.
+#' 
+#' It includes a few other convenience functions (such as gammaFromModeSD) that probably belong elsewhere.
+#' 
+#' To apply the psychophysical functions we typically apply to these kinds of situations, install dlandy/classicPsychophysics
+#' 
+#' 
+#' To see a full list of functions, do ls("package:demographicModeling")
+#' 
+#'
+#' @name demographicModeling
+#' @docType package
+NULL
